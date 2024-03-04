d8e2e41b2d994cfb3fa8cc436c7025845e4e5072
Fitri HY <155282015+fitri-hy@users.noreply.github.com>
2024-03-04 11:19:25 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,9 +35,6 @@ const Footer = () => {
 						<i class="fab fa-facebook-square"></i>
 					</button>
 					<button onClick={() => { window.location.href = "https://github.com/fitri-hy"; }} class="bg-indigo-400 text-white shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-md outline-none focus:outline-none mr-2" type="button">
-						<i class="fab fa-dribbble"></i>
-					</button>
-					<button onClick={() => { window.location.href = "https://github.com/fitri-hy"; }} class="bg-indigo-400 text-white shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-md outline-none focus:outline-none mr-2" type="button">
 						<i class="fab fa-github"></i>
 					</button>
 				</div>
